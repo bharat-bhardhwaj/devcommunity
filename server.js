652edc5b66261f95a937431e75ed224fb415ebd2
bharat bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-06-07 11:50:46 +0530

    connect routes to the seperated file

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express =require('express');
+const connectDB= require('./config/db');
 
 const app=express();
 
-const PORT =process.env.PORT || 5000
+//connect database
+
+connectDB();
+
+const PORT =process.env.PORT || 5000;
+
+
+//Define routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/profile',require('./routes/api/profile'));
+
+
+
+
 
 app.get('/', (req,res)=>res.send("api running"));
+
+
 
 
 app.listen(PORT, ()=>console.log(`Server started on post ${PORT}`));
