fa2a749d5629e37df65b36c12b0542ba6253fc0a
bharat bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-06-08 15:14:40 +0530

    get all post and post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -48,4 +48,50 @@ check('text','Text is required').not().isEmpty()
 });
 
 
+// @route GET api/posts
+// @desc Get all post
+// @access private
+
+
+router.get('/',auth,async (req,res)=>{
+        try {
+
+            const posts = await Post.find().sort({data:-1})
+
+            res.json(posts);
+            
+        } catch (err) {
+            console.error(err.msg);
+            res.status(500).send('server error')
+        }
+})
+
+
+
+// @route GET api/posts
+// @desc Get post by id
+// @access private
+
+
+router.get('/:id',auth,async (req,res)=>{
+    try {
+
+        const post = await Post.findById(req.params.id);
+
+        if(!post){
+            return res.status(404).json({msg:'Post not found'})
+        }
+
+        res.json(post);
+        
+    } catch (err) {
+        if(err.kind==='ObjectId'){
+            
+            return res.status(404).json({msg:'Post not found'})
+        }
+        res.status(500).send('server error')
+    }
+})
+
+
 module.exports =router;
