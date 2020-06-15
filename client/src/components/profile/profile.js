a9af829136e1a68c4715067f5c49462cdec3158c
bharat bhardhwaj <bharatbhardhwaj571@gmail.com>
2020-06-15 19:36:10 +0530

    now we can the experiences on me profile

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
 
 import { getProfilebyId } from '../../actions/profile';
 
@@ -29,6 +30,19 @@ const Profile = ({ getProfilebyId, profile: { profile, loading },auth, match }) 
           <div className="profile-grid my-1">
               <ProfileTop profile={profile}/>
               <ProfileAbout profile={profile}/>
+              <div className="profile-exp bg-white p-2">
+                  <h2 className="text-primary">Experience</h2>
+                  {profile.experience.length>0 ? (
+                  <Fragment>
+                      {profile.experience.map(experience =>(
+
+                          <ProfileExperience 
+                          key={experience._id} 
+                          experience={experience}/>
+
+                      ))}
+                          </Fragment>): (<h4>No experience Creddentials</h4>)}
+              </div>
               
           </div>
     </Fragment>}
